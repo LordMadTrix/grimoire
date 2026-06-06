f4e43458c4e3a6c798d08eade1f673eb0de77974
LordMadTrix <LordMadTrix@users.noreply.github.com>
2026-06-06 21:53:34 +0200

    Clean up useless scripts and scraping files

--- map-editor/find_stamp_info.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-const urls = JSON.parse(fs.readFileSync('./inkarnate_urls.json', 'utf-8'));
-const entry = urls.find(u => JSON.stringify(u).includes('gmua4v7ktx5iobfgn3aw0trqpvyw'));
-console.log("Entry in inkarnate_urls.json:", entry);
